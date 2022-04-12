8676ba8e35aa91bda6fe6a7af6129b6702f46e27
김대현 <rick083808@gmail.com>
2022-04-12 22:48:25 +0900

    [FIX] 본문 추가

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,15 +1,15 @@
 package com.example.demo.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.io.*;
 import java.net.*;
@@ -24,27 +24,27 @@ public class DemoController {
     //GET, POST등의 method할 때 Get임
     @GetMapping("hello") // "/hello"를 입력하면 method가 호출됨
     public String hello(Model model) {
-        model.addAttribute("data","hello!!");
+        model.addAttribute("data", "hello!!");
         return "hello";
     }
 
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model){
+    public String helloMvc(@RequestParam("name") String name, Model model) {
         //parameter를 요청 , 정보를 담아서 view로델 보낼 모델
 
-        model.addAttribute("name",name);// 받은 파라미터를 이용해서 name키의 값으로 지정
+        model.addAttribute("name", name);// 받은 파라미터를 이용해서 name키의 값으로 지정
         return "hello-template"; //hello-template.html로
     }
 
     @GetMapping("hello-string")
     @ResponseBody // http의 body부분에 이 데이터를 직접 넣어 주겠다는 의미
-    public String helloString(@RequestParam("name") String name){
+    public String helloString(@RequestParam("name") String name) {
         return "hello" + name; //viewResolver가 아니고 data가 그대로 전달됨
     }
 
     @GetMapping("hello-api")
     @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name){
+    public Hello helloApi(@RequestParam("name") String name) {
         Hello hello = new Hello(); //Hello객체 생성
 
         hello.setName(name); //파라미터로 넘어온 name을 이용하여 데이터를 넣음
@@ -55,26 +55,26 @@ public class DemoController {
         private String name;
 
         //getter -> getName함수로 데이터를 꺼내 사용
-        public String getName(){
+        public String getName() {
             return name;
         }
 
         //setter -> setName함수로 데이터를 입력
-        public void setName(String name){
+        public void setName(String name) {
             this.name = name;
         }
     }
 
     @GetMapping("naver")
     @ResponseBody
-    public String naverApi(@RequestParam("name") String name){
+    public String naverApi(@RequestParam("name") String name) throws IOException {
         Naver naver = new Naver(); //Hello객체 생성
         return naver.search(name);//파라미터로 넘어온 name을 이용하여 데이터를 넣음
     }
 
 
     static class Naver {
-        public String search(String searchWord) {
+        public String search(String searchWord) throws IOException {
             String text = null;
             try {
                 text = URLEncoder.encode(searchWord, "UTF-8");
@@ -89,7 +89,7 @@ public class DemoController {
             requestHeaders.put("X-Naver-Client-Id", "0mjo6hkJAsuDzP5fU6gS");
             requestHeaders.put("X-Naver-Client-Secret", "LcuKnQsz3r");
             String responseBody = get(apiURL, requestHeaders);
-            System.out.println(responseBody);
+//            System.out.println(responseBody);
             ObjectMapper mapper = new ObjectMapper();
             Map<String, Object> map = new HashMap<>();
             try {
@@ -100,14 +100,20 @@ public class DemoController {
             }
 
             ArrayList<Map<String, Object>> mapList = (ArrayList<Map<String, Object>>) map.get("items");
-            for(int i=0; i < mapList.size(); i++) {
+            for (int i = 0; i < mapList.size(); i++) {
                 System.out.println(mapList.get(i).get("link"));
-                mapList.get(i).put("title", "t");
-                System.out.println(mapList.get(i));
+
+//                mapList.get(i).put("title", "t");
+//                System.out.println(mapList.get(i));
             }
 
-            return responseBody;
+//            String html = getHTML("https://blog.naver.com//PostView.naver?blogId=papakang3156&logNo=222692642625&from=search&redirect=Log&widgetTypeCall=true&directAccess=false");
+            String html = getMeta("https://blog.naver.com/papakang3156?Redirect=Log&logNo=222692642625");
+//            System.out.println(html);
+
+            return html;
         }
+
         private String get(String apiUrl, Map<String, String> requestHeaders) {
             HttpURLConnection con = connect(apiUrl);
             try {
@@ -155,56 +161,50 @@ public class DemoController {
         }
     }
 
-//    class
+    public static String getHTML(String urlToRead) {
+        String result = "";
+        try {
+            URL url = new URL(urlToRead);
 
+//            System.out.println("url=[" + url + "]");
+//            System.out.println("protocol=[" + url.getProtocol() + "]");
+//            System.out.println("host=[" + url.getHost() + "]");
+//            System.out.println("content=[" + url.getContent() + "]");
 
-//    @GetMapping("blog")
-//    @ResponseBody
-//    public String blog(@RequestParam("name") String name){
-//        PostURL postURL = new PostURL(); //Hello객체 생성
-//        postURL(name);
-//        //파라미터로 넘어온 name을 이용하여 데이터를 넣음
-//    }
-//
-//    public class PostURL {
-//        public void search(String) {
-//            try {
-//                String param = "name=" + URLEncoder.encode("미니", "UTF-8");
-//
-//                URL url = new URL(name);
-//                URLConnection conn = url.openConnection();
-//
-//                conn.setDoOutput(true);
-//                conn.setUseCaches(false);
-//                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-//
-//                DataOutputStream out = null;
-//
-//                try {
-//                    out = new DataOutputStream(conn.getOutputStream());
-//                    out.writeBytes(param);
-//                    out.flush();
-//                } finally {
-//                    if (out != null) out.close();
-//                }
-//
-//                InputStream is = conn.getInputStream();
-//                Scanner scan = new Scanner(is);
-//
-//                int line = 1;
-//                while (scan.hasNext()) {
-//                    String str = scan.nextLine();
-//                    System.out.println((line++) + ":" + str);
-//                }
-//                scan.close();
-//
-//            } catch (MalformedURLException e) {
-//                System.out.println("The URL address is incorrect.");
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                System.out.println("It can't connect to the web page.");
-//                e.printStackTrace();
-//            }
-//        }
-//    }
+            InputStream is = url.openStream();
+            int ch;
+            while ((ch = is.read()) != -1) {
+//                System.out.print((char) ch);
+                result += (char) ch;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static String getMeta(String url) throws IOException {
+        // 네이버 블로그 url로 document가져오기
+        Document doc = Jsoup.connect(url).get();
+
+        // iframe 태그에 있는 진짜 블로그 주소 가져오기
+        org.jsoup.select.Elements iframes = doc.select("iframe#mainFrame");
+        String src = iframes.attr("src");
+        //진짜 블로그 주소 document 가져오기
+        String url2 = "http://blog.naver.com" + src;
+        Document doc2 = Jsoup.connect(url2).get();
+//        System.out.println("주소 확인용 : " + url2);
+//        System.out.println("doc2 : " + doc2);
+        // 블로그에서 원하는 블로그 페이지 가져오기
+        String[] blog_logNo = src.split("&");
+        String[] logNo_split = blog_logNo[1].split("=");
+        String logNo = logNo_split[1];
+
+        // 찾고자 하는 블로그 본문 가져오기
+        String real_blog_addr = "div#post-view" + logNo;
+        String blog_element = String.valueOf(doc2.select(real_blog_addr));
+        return blog_element;
+    }
 }
